581ac15145bbbcf8a6f3da7f8f7175b9d139c805
testIversoft <38438691+testIversoft@users.noreply.github.com>
2018-04-16 22:18:35 -0400

    Changes in stop process

--- utilites/applications.stop.sh
+++ utilites/applications.stop.sh
@@ -1,3 +1,4 @@
 # stop consul
 ps -ef | grep consul | grep -v grep | awk '{print $2}' | xargs kill -9
-
+ps -ef | grep UserManagementService | grep -v grep | awk '{print $2}' | xargs kill -9
+ps -ef | grep RouterService | grep -v grep | awk '{print $2}' | xargs kill -9
